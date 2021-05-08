004c078e9a6a53c2df70e70e3023a1bd7d33e1cf
natewilliams1 <nmwilliams87@gmail.com>
2021-05-08 15:42:55 -0400

    homework_refactor

--- lib/pages/account_home_page.rb
+++
@@ -1,8 +0,0 @@
-class AccountHomePage
-  include PageObject
-
-  page_url 'https://shop.shipt.com/'
-
-  link(:cart_link, href: '/cart')
-  link(:account_link, href: '/account/profile/')
-end
---
+++ lib/pages/shipt_account_home_page.rb
@@ -0,0 +1,8 @@
+class ShiptAccountHomePage
+  include PageObject
+
+  page_url 'https://shop.shipt.com/'
+
+  link(:cart_link, href: '/cart')
+  link(:account_link, href: '/account/profile/')
+end
